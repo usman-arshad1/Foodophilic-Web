00928403f42e79898d46531d0a3647ae5810bd34
Usman Arshad <uarshad2012@gmail.com>
2023-03-12 00:42:36 -0800

    change

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -4,22 +4,23 @@ import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 
 function Main() {
-    return (
-        // <div className='background'>
-            <div className="search">
-                <IconButton  aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
-                <InputBase
-                    // sx={{ ml: 1, flex: 1 }}
-                    placeholder="Search"
-                />
-                <IconButton type="button" aria-label="search">
-                    <SearchIcon />
-                </IconButton>
-            </div>
-        /* </div> */
-    )
+  return (
+    <div className='background'>
+        <div className="search">
+            <IconButton sx={{ p: '10px' }} aria-label="menu">
+                <MenuIcon />
+            </IconButton>
+            <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Search"
+                
+            />
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                <SearchIcon />
+            </IconButton>
+        </div>
+    </div>
+  );
 }
 
 export default Main
