d72fc9fdbd4a2e665b49008e5457bc4a98c45a39
Usman Arshad <uarshad2012@gmail.com>
2023-03-11 20:39:15 -0800

    profile current update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { SideNavigation } from './components/SideNavigation'
+import Profile from './views/Profile'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
   const jsonThing = {
     name: "usman",
-    gender: "female"
+    gender: "female",
+    numPosts: 7
   }
   return (
     <div className="App">
-        <SideNavigation json={jsonThing} />
+      {/* <SideNavigation json={jsonThing} /> */}
+      <Profile user={jsonThing.name} username={jsonThing.gender} numberPosts={jsonThing.numPosts}/>
     </div>
   )
 }
