e64094c0cc09ee286cf18db3827e3cc487f0249d
Willy Liao <willyliao0425@gmail.com>
2023-03-12 05:58:09 -0700

    feat: fixed firebase provider and main now displays dynamic posts

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -7,33 +7,71 @@ import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import Button from "@mui/material/Button";
 import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import { getDocs, getFirestore, collection } from "firebase/firestore";
+import {
+  FirebaseAppProvider,
+  FirestoreProvider,
+  useFirestoreDocData,
+  useFirestore,
+  useFirebaseApp,
+  useFirestoreCollection,
+} from "reactfire";
+import { useState, useEffect } from "react";
 
 function Main() {
-  const posts = [
-    {
-      user: {
-        name: "Linus Tech Tips",
-        profileImg: "src/assets/king-slime.webp",
-      },
-      post: {
-        time: "3h ago",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
-        foodImg: "./src/assets/pizza-with-pineapple-and-thin-crust.jpg",
-      },
-    },
-    {
-      user: {
-        name: "Linus Tech Tips",
-        profileImg: "src/assets/king-slime.webp",
-      },
-      post: {
-        time: "3h ago",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
-        foodImg: "./src/assets/pizza-with-pineapple-and-thin-crust.jpg",
-      },
-    },
-  ];
+  const [posts, setPosts] = useState([]);
 
+  // const posts = [
+  //   {
+  //     user: {
+  //       name: "Linus Tech Tips",
+  //       profileImg: "src/assets/king-slime.webp",
+  //     },
+  //     post: {
+  //       time: "3h ago",
+  //       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+  //       foodImg: "./src/assets/pizza-with-pineapple-and-thin-crust.jpg",
+  //     },
+  //   },
+  //   {
+  //     user: {
+  //       name: "Linus Tech Tips",
+  //       profileImg: "src/assets/king-slime.webp",
+  //     },
+  //     post: {
+  //       time: "3h ago",
+  //       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+  //       foodImg: "./src/assets/pizza-with-pineapple-and-thin-crust.jpg",
+  //     },
+  //   },
+  // ];
+
+  const firestore = useFirestore();
+
+  useEffect(() => {
+    // const userCollection = collection(firestore, "users");
+    // getDocs(userCollection).then((users) => {
+    //   users.forEach((user) => {
+    //     console.log("user", user.data());
+    //   });
+    // });
+
+    // use for posts
+    const postCollection = collection(firestore, "posts");
+    getDocs(postCollection).then((res) => {
+      setPosts([...res.docs]);
+      // console.log(posts.docs);
+      // posts.docs.forEach(data => console.log(data.data()))
+    });
+  }, []);
+
+  const displayPosts = () => {
+    console.log("----------------\n----------------");
+    posts.forEach((post) => {
+      console.log(post.data());
+    });
+  };
+  displayPosts();
   return (
     <div className="main-container">
       <div className="main-shit">
@@ -61,8 +99,8 @@ function Main() {
 
       <div className="profileContainer">
         <div className="user-posts">
-          {posts.map((post) => (
-            <Post user={post.user} post={post.post} />
+          {posts.map((post, index) => (
+            <Post key={index} post={post.data()} />
           ))}
         </div>
       </div>
