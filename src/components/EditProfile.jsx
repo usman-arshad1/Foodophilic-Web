882fe593ad3a7759c0fcfd6a118c8c4d23f36d42
Winston <winston_nguyenn@hotmail.com>
2023-03-11 22:08:55 -0800

    Fixed styling classname conflicts

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import "./../styles/popUp.css";
 
 const EditProfile = () => {
@@ -21,19 +22,19 @@ const EditProfile = () => {
                 <div className='imageButton'>+ Upload Image</div>
             </div>
             <span className='inlineImage componentProfileEdit'>
-                <div className='profilePic'><img src="https://dummyimage.com/600x400/000/fff" alt="Porilfe Pic"></img></div>
+                <div className='profilePic'><img src="https://dummyimage.com/500x500/000/fff" alt="Porilfe Pic"></img></div>
                 <div className='usernameInfo editBoxIn'>
                     <label className='editLabel'>Username</label>
                     <textarea className='usernameInput popUpTextArea' placeholder='Enter username here' rows={1} maxLength={25}>Get username here
                     </textarea>
                 </div>
             </span>
-            <div className='email editBox componentProfileEdit'>
+            <div className='emailInfo editBox componentProfileEdit'>
                 <label className='editLabel'>Email</label>
                 <textarea className='emailInfo popUpTextArea' placeholder='Enter email here' rows={2} maxLength={255}>Get email here
                 </textarea>
             </div>
-            <div className='bio editBox componentProfileEdit'>
+            <div className='bioInfo editBox componentProfileEdit'>
                 <label className='editLabel'>Bio</label>
                 <textarea className='bioInfo popUpTextArea' placeholder='Enter bio here (Max 255 characters)' rows={5} maxLength={255}>Get bio here
                 </textarea>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { EditProfile } from './components/editProfile'
+import { CreatePost } from './components/CreatePost'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <EditProfile/>
+    <CreatePost />
   </React.StrictMode>,
 )
