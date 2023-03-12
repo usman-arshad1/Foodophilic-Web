73ae290cc08f8b6b5e5dea4946abe619b0671502
Willy Liao <willyliao0425@gmail.com>
2023-03-11 22:58:16 -0800

    fix: fixed layout of Profile to fit as an Outlet for MainLayout

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,23 +1,49 @@
 import "./../styles/profile.css";
 
 function Profile({ user, username, numberPosts }) {
-    return (
-        <div className='profile'>
-                    <img src=".\src\assets\banhmi.png" alt="" />
-            <div className='head'>
-                <img src='.\src\assets\istockphoto-1223671392-612x612.jpg' alt=""/>
-                <div className="edit">
-                    <button type="button">Edit Profile</button>
-                </div>
-                <div className='profile_name'>{user}</div>
-                <div className='username'>@{username}</div>
-                <div className='numberOfPosts'>{numberPosts} Posts</div>
-                <div className="bio">I am Sim, the Simmiest of rattiest of all Sims, ruler of Simtarctica.</div>
-            </div>
-            <div className="divider"></div>
-            <div className="body"></div>
+  return (
+    <div className="profile-container">
+      <div className="profile-header">
+        <div className="banner-image">
+          <img src=".\src\assets\banhmi.png" alt="banner image" />
         </div>
-    )
+
+        <div className="profile-header-info-container">
+          <div className="profile-photo-button-container">
+            <img
+              className="profile-photo"
+              src=".\src\assets\istockphoto-1223671392-612x612.jpg"
+              alt=""
+            />
+            <div className="edit-button-wrapper">
+              <button className="edit-button" type="button">
+                Edit Profile
+              </button>
+            </div>
+          </div>
+          <div className="profile-header-info">
+            <div className="profile-name-post-number-container">
+              <div className="profile-name">{user}</div>
+              <div className="post-number">
+                <span>{numberPosts}</span> Posts
+              </div>
+            </div>
+
+            <div className="username">@{username}</div>
+
+            <div className="bio">
+              I am Sim, the Simmiest of rattiest of all Sims, ruler of
+              Simtarctica.
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="divider"></div>
+
+      <div className="body"></div>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
