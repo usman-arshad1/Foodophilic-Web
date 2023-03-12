39485f3d313a026804d2a34eabc0f27634d34d6c
Sumit Khanduri <veerkhanduri@gmail.com>
2023-03-11 17:29:34 -0800

    finished the post card

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { SideNavigation } from './components/SideNavigation'
+import { Post } from './components/PostCard'
 import './App.css'
 
 function App() {
+  
   const [count, setCount] = useState(0)
-  const jsonThing = {
-    name: "usman",
-    gender: "female"
+  const user1 = {
+    name: "Linus Tech Tip",
+    profileImg: "src/assets/404-4042710_circle-profile-picture-png-transparent-png.png",
+    
+  }
+
+  const post1 = {
+
+    time: "3h ago",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+    foodImg: "src/assets/pizza-with-pineapple-and-thin-crust.jpg"
   }
   return (
     <div className="App">
-        <SideNavigation json={jsonThing} />
+        <Post user={user1} post = {post1} />
     </div>
   )
 }
